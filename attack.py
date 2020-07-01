e92c501c3b3461649ab2ddd11994867bed80d450
J Ninja <j@optimistic.ninja>
2020-07-01 14:21:29 -0700

    Need to fix viable_base_charset for partial keys.

--- attack.py
+++ attack.py
@@ -10,7 +10,7 @@ from functools import reduce
 
 from argparse import ArgumentParser
 from const import B16, B32, B64, BASE_DEFAULT_CHARSETS, \
-    MONOGRAMS, DIGRAMS, TRIGRAMS, HEXAGRAMS
+    MONOGRAMS, DIGRAMS, TRIGRAMS, HEXAGRAMS, NGRAM_CHAR_LEN, NGRAMS_DECRYPT_MAPPING
 from output import eprintc
 
 parser = ArgumentParser(description='Attack message encoded in I Ching ngrams')
@@ -22,6 +22,39 @@ parser.add_argument('-hk', '--hexagram-key', help='hexagram key or partial if kn
 #                    nargs='?', const=True, default=False)
 # parser.add_argument('-te', '--target-encoding', help='character encoding to target')
 ns = parser.parse_args()
+
+
+def deduce_ngram_type(char: str) -> str:
+    """
+    Deduce ngram type from character
+    :param char: first character from encrypted message
+    :return: one of ['tri', 'di', 'mono']
+    """
+    if char in TRIGRAMS:
+        return 'tri'
+    elif char in DIGRAMS:
+        return 'di'
+    elif char in MONOGRAMS:
+        return 'mono'
+    elif char in HEXAGRAMS:
+        return 'hex'
+    else:
+        raise ValueError("invalid message for decryption")
+
+
+def translate_ngrams_to_hexagrams(encrypted: str, ngram_type: str, encoding: str) -> bytes:
+    """
+    Translate monograms, digrams and trigrams to hexagrams for intermediate mapping before decrypt
+    :param encrypted: monograms, digrams or trigrams as bytes
+    :param ngram_type: 'mono' or 'di' or 'tri'
+    :return: hexagrams as bytes
+    """
+    char_len = NGRAM_CHAR_LEN[ngram_type]
+    translated = ''
+    for ngram_grouping in [encrypted[y - char_len: y]
+                           for y in range(char_len, len(encrypted) + char_len, char_len)]:
+        translated += NGRAMS_DECRYPT_MAPPING[ngram_type][ngram_grouping]
+    return translated
 
 
 def deduce_bases(message: str) -> list:
@@ -83,7 +116,7 @@ def validate_base_key(base_key: str):
     :param base_key:
     :return: set of viable character sets
     """
-    viable_charsets = set()
+    viable_charsets = []
     for v in BASE_DEFAULT_CHARSETS.values():
         for c in base_key:
             if c not in v:
@@ -91,7 +124,7 @@ def validate_base_key(base_key: str):
         replaced = v
         for c1 in base_key:
             replaced = replaced.replace(c1, '')
-        viable_charsets.add(replaced)
+        viable_charsets.append(replaced)
     if len(viable_charsets) == 0:
         eprintc('invalid base key', fail=True)
     return viable_charsets
@@ -127,37 +160,85 @@ def attempt_decode(base: int, permutation: str, encoding: str):
     :param encoding: target character encoding
     """
     if base == B16:
-        b64.b16decode(permutation).decode(encoding)
+        decoded = b64.b16decode(permutation).decode(encoding)
     elif base == B32:
-        b64.b32decode(permutation).decode(encoding)
+        decoded = b64.b32decode(permutation).decode(encoding)
     elif base == B64:
-        b64.b64decode(permutation).decode(encoding)
+        decoded = b64.b64decode(permutation).decode(encoding)
+    return decoded
+
+
+def permute_bases_charsets(bases: list, viable_base_charsets: list) -> list:
+    """
+    Reduce bases charsets by removing known keys
+    :param bases: bases to target
+    :param viable_base_charsets: base charsets to target
+    :return:
+    """
+    base_charset_partials = set()
+    bases_permutations = []
+    for base, viable_charset in zip(bases, viable_base_charsets):
+        if viable_charset == '':
+            bases.remove(base)
+            viable_base_charsets.remove(viable_charset)
+            eprintc('key longer than base, removing base%d' % base, warn=True)
+            continue
+        if len(viable_charset) != base:
+            replaced = BASE_DEFAULT_CHARSETS[base]
+            for c1 in viable_charset:
+                for c2 in BASE_DEFAULT_CHARSETS[base]:
+                    if c1 != c2:
+                        replaced = replaced.replace(c2, '')
+            base_charset_partials.add(replaced)
+    for base, partial, viable_base_charset in zip(bases, base_charset_partials, viable_base_charsets):
+        if len(partial) > 1:
+            partial_permutations = permutations(partial)
+            full_permutations = []
+            for permutation in partial_permutations:
+                full_permutations.append(viable_base_charset + permutation)
+            bases_permutations += permutations(partial)
+        elif len(partial) == 1:
+            bases_permutations += [viable_base_charset + partial]
+    return bases_permutations \
+        if len(bases_permutations) != 0 else [permutations(b) for b in viable_base_charsets]
+
+
+def permute_hexagrams(known: str):
+    hexagrams_permutations = []
+    if known == HEXAGRAMS:
+        hexagrams_permutations = permutations(HEXAGRAMS)
+    else:
+        partial = ''.join(c for c in HEXAGRAMS if c not in known)
+        partial_permutations = permutations(partial)
+        for permutation in partial_permutations:
+            hexagrams_permutations.append(known + ''.join(permutation))
+    return hexagrams_permutations
 
 
 # O(scary)
 # TODO: Write keys for each permutation, add date to file
-def attack(message: str, viable_base_charsets: set, ngram_alphabet: str):
+def attack(message: str, viable_base_charsets: list, hexagram_charset: str):
     """
     Attack the encrypted message
     :param message: encrypted message
     :param viable_base_charsets: set of viable bases
-    :param ngram_alphabet: character set for ngrams
+    :param hexagram_charset: character set for hexagrams
     """
     bases = deduce_bases(message)
-    bases_permutations = [permutations(b) for b in viable_base_charsets]
-    ngrams_permutations = permutations(ngram_alphabet)
+    bases_permutations = permute_bases_charsets(bases, viable_base_charsets)
+    hexagrams_permutations = permute_hexagrams(hexagram_charset)
     average = 0
     i = 0
-    for rp in ngrams_permutations:
+    for hexagram_permutation in hexagrams_permutations:
         base_offset = 0
-        for bp in bases_permutations:
+        for base_permutations in bases_permutations:
             base = bases[base_offset]
-            for ap in bp:
+            for base_charset_permutation in base_permutations:
                 start = time.time()
-                mapping = dict(zip(list(rp), list(ap)))
+                mapping = dict(zip(list(hexagram_permutation), list(base_charset_permutation)))
                 permutation = message
-                for k, v in mapping.items():
-                    permutation = permutation.replace(k, v)
+                for b, viable_charset in mapping.items():
+                    permutation = permutation.replace(b, viable_charset)
                 try:
                     stop = time.time()
                     # TODO: Adjust calculation for partial known mapping
@@ -167,9 +248,8 @@ def attack(message: str, viable_base_charsets: set, ngram_alphabet: str):
                     days_left = (average / i) // (24 * 3600)
                     eprintc('Days Left: %s' % days_left, important=True, one_line=True)
                     # TODO: Enable encoding target
-                    attempt_decode(base, permutation, 'utf-8')
-                    with open('permutations.txt', 'a') as f:
-                        f.write(permutation + '\n')
+                    decoded = attempt_decode(base, permutation, 'utf-8')
+                    eprintc('\nFound: ' + decoded, important=True)
                 except binascii.Error:
                     continue
                 except UnicodeDecodeError:
@@ -181,6 +261,6 @@ def attack(message: str, viable_base_charsets: set, ngram_alphabet: str):
 
 if __name__ == '__main__':
     viable_base_charsets = validate_args()
-    ngram_charset = deduce_ngrams(ns.message[0])
-    attack(ns.message, viable_base_charsets, ngram_charset)
+    message = translate_ngrams_to_hexagrams(ns.message, deduce_ngram_type(ns.message[0]), 'utf-8')
+    attack(message, viable_base_charsets, HEXAGRAMS if not ns.hexagram_key else ns.hexagram_key)
 
